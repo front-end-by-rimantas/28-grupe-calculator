db22821f87cd345f5ea19624f8843f0279c9f640
Rimantas <belauzas@yahoo.com>
2021-06-01 18:30:39 +0300

    Keyboard data for rendering

--- js/components/Calculator.js
+++ js/components/Calculator.js
@@ -3,6 +3,28 @@ class Calculator {
         this.selector = selector;
 
         this.DOM = null;
+        this.buttonsData = [
+            { title: 'C', color: 'grey' },
+            { title: '+/-', color: 'grey' },
+            { title: '%', color: 'grey' },
+            { title: '/', color: 'dark' },
+            { title: '1', color: 'white' },
+            { title: '2', color: 'white' },
+            { title: '3', color: 'white' },
+            { title: '*', color: 'dark' },
+            { title: '4', color: 'white' },
+            { title: '5', color: 'white' },
+            { title: '6', color: 'white' },
+            { title: '-', color: 'dark' },
+            { title: '7', color: 'white' },
+            { title: '8', color: 'white' },
+            { title: '9', color: 'white' },
+            { title: '+', color: 'dark' },
+            { title: '0', color: 'white' },
+            { title: '.', color: 'white' },
+            { title: '<', color: 'white' },
+            { title: '=', color: 'red' },
+        ];
 
         this.init();
     }
